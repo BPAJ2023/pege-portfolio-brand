d211509be4d66a61e1096c01f6329214bf3d2d9a
Bartek Pośpiech <patka11nero@gmail.com>
2023-06-05 14:21:28 +0200

    first commit create repo and bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new miniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/,
+        use: [
+          { loader: miniCssExtractPlugin.loader},
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  }
+}
